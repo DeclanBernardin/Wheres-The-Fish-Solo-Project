bd8eb69298fef6ffba6ddc30d72fe2de5539a818
Declan Bernardin <dmjbernardin@yahoo.com>
2019-09-10 08:53:24 -0500

    added api key and google maps api is up and running

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,42 +1,33 @@
 import React, {Component} from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-import { compose, withProps } from 'recompose';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
-class InfoPage extends Component {
-
- 
-  
-    
-
-  render() {
+class MainPage extends Component{
+  render(){
     const MyMapComponent = withScriptjs(withGoogleMap((props) =>
       <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-      >
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-      </GoogleMap>
+        defaultZoom={6}
+        defaultCenter={{ lat: 46.392410, lng: -94.636230 }}
+      />
     ))
     return(
+      <MyMapComponent
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
       
-      <div>
-        <h1>main page!</h1>
-        < MyMapComponent
-    isMarkerShown
-    googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-    loadingElement = {<div style = {{ height: `100%` }} />}
-    containerElement = {<div style = {{ height: `400px` }} />}
-    mapElement = {<div style = {{ height: `100%` }} />}
-    />
-    
-      </div>
     );
   }
 }
 
 
-export default InfoPage;
+
+  
+
+
+export default MainPage;
