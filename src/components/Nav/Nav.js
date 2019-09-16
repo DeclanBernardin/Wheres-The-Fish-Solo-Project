8a4752864fa652f732a77a342a6b39457176b177
Declan Bernardin <dmjbernardin@yahoo.com>
2019-09-16 09:47:58 -0500

    added image

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Where The Fish</h2>
+      <h2 className="nav-title">WTF</h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
